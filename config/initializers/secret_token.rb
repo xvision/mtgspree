d34d3a9cd51aae8e298fc555ea53ee80037dc5ec
xvision <xvision@2013.goyal.me>
2013-11-07 02:10:52 +0530

    First Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mtgspree::Application.config.secret_key_base = '0e0131f0c16b9f6dcc55cd661d3b0919af76548d773a7f03b588567067fa73c18f791ce3012fe670ab07d8e2f657bcdc4b28bb38ad4ba620db312b209824b9dd'
